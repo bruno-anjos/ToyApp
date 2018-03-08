bcfbf8a9f8efbcba5a869cb71785e6dc53d0151e
Nuno Morais <nm.morais@campus.fct.unl.pt>
2018-03-08 12:44:36 +0000

    Code Reformat and removing "average per minute"

--- App/toyapp.py
+++ App/toyapp.py
@@ -3,8 +3,10 @@ import time
 import hashlib
 import sys
 
+#CONST_USAGE = "Usage: [insertions per second] [averages per second] [timeout in seconds]"
 
-CONST_USAGE = "Usage: [insertions per second] [averages per second] [timeout in seconds]"
+
+CONST_USAGE = "Usage: [insertions per second] [timeout in seconds]"
 
 
 
@@ -13,34 +15,43 @@ def main(argv):
 	#Feeds the random generator with the current UNIX timestamp as seed
 	random.seed(time.time())
 
-	if len(argv) != 4:
+	if len(argv) != 3:
 		print(CONST_USAGE)
 		sys.exit(1)
 
 	insertPerMin = argv[1]
-	avgPerMin = argv[2]
 	timeout = int(argv[3]) * 10000
-
 
 
 	print("starting main loop")
 	mainLoop(insertPerMin, avgPerMin, timeout)
 
 	sys.exit(0)
-	
+
+
+# Converts @param timesPerMin to how much time to wait in order to 
+# have @param timesPerMin happen that many times per minute
+def getSleepTime(timesPerMin):
+	return 60000 / timesPerMin
+
+
 
 # main script loop, logs start time and runs until timeout expires
 # every x seconds will insert a tuple into the database
-def mainLoop(insertPerMin, avgPerMin, timeout):
+def mainLoop(insertPerMin, timeout):
 
 	startTime = time.time()
+	sleepTime = getSleepTime(insertPerMin)
 
 	while(True):
 
+		time.sleep(insertPerMin)
 		if startTime - time.time() > timeout :
 			break 
 
-
+		key = getHash()
+		value = getNum()
+		insertIntoDB(key,value)
 
 
 #Returns a hash using SHA256 algorithm and the current UNIX timestamp
@@ -58,11 +69,15 @@ def getNum(min, max):
 
 #inserts the @param key and @param value into the database
 def insertIntoDB(key, value):
-	return None
+	return None #TODO
 
-# gets the average from all the tuples and returns it
+# gets the average from all the tuples in the databse and returns it
 def getAverage():
-	return None
+	return None #TODO
+
+# connects to the database
+def setupDatabase():
+	return None #TODO
 
 
 #conventional stuff, dont ask me.
