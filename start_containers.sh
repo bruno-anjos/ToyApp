20775c5b681805700d58e54ef0d1bc6fb470b121
Nuno Morais <nm.morais@campus.fct.unl.pt>
2018-03-09 11:48:46 +0000

    Added script documentation

--- start_containers.sh
+++ start_containers.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-nContainers=$1
-insPerMin=$2
-timeout=$3
-counter=0
-imageName=$(cat logs/image_name.log)
-command="bash init_container.sh $insPerMin $timeout"
-echo $command
+nContainers=$1 # the number of containers to open
+insPerMin=$2 # number of insertions per minute in the python app
+timeout=$3 # the python app timeout
+imageName=$(cat logs/image_name.log) # fetches the image name in the logs file
+command="bash init_container.sh $insPerMin $timeout" # built command to pass to the container
+counter=0 
 
 while [ $counter -lt $nContainers ]; do
-	containerName="container$counter"
+	containerName="Container$counter" 
 	echo "Starting $containerName" 
 	docker run -d  --name $containerName $imageName $command
 	((counter++))
