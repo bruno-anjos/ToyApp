0d92d07be4c6dde06f1f7282788f86b2cbdf330f
An0nZero <bruno.anjos96@hotmail.com>
2018-03-17 13:44:29 +0000

    Update
    Added remote db connections (incremental IP), remote db insertions, batch size functionality (multiple values in one SQL insertion)

--- Container/toyapp.py
+++ Container/toyapp.py
@@ -3,11 +3,13 @@ import time
 import hashlib
 import sys
 import MySQLdb
+import ipaddress
+from collections import deque
 
 # CONST_USAGE = "Usage: [insertions per second] [averages per second] [timeout in seconds]"
 
 
-CONST_USAGE = "Usage: [insertions per minute] [timeout in minutes]"
+CONST_USAGE = "Usage: python3 toyapp.py [insertions per minute] [max insertions] [number of clients] [starting ip] [batch size]"
 CONST_DB_HOST = "localhost"
 CONST_DB_USER = "root"
 CONST_DB_PASSWORD = "toor"
@@ -19,6 +21,7 @@ CONST_MIN_NUM = 1
 CONST_MAX_NUM = 100
 
 
+
 def main(argv):
 
     print("Starting toyapp")
@@ -27,15 +30,18 @@ def main(argv):
 
     #testDatabase()
 
-    if len(argv) != 3:
+    if len(argv) != 4:
         print(CONST_USAGE)
         sys.exit(1)
 
-    insertPerMin = argv[1]
+    insertPerMin = int(argv[1])
     maxInsertions = int(argv[2])
+    numClients = int(argv[3])
+    startingIP = ipaddress.ip_address(argv[4])
+    batchSize = int(argv[5])
 
     print("Starting main loop")
-    mainLoop(insertPerMin, timeout)
+    mainLoop(insertPerMin, maxInsertions, numClients, startingIP, batchSize)
 
     sys.exit(0)
 
@@ -49,29 +55,44 @@ def getSleepTime(timesPerMin):
 # main script loop, runs until it has inserted @arg maxInsertions tuples
 # every x seconds will insert a tuple into the database
 # where x is how many times it inserts per minute
-def mainLoop(insertPerMin, maxInsertions):
+def mainLoop(insertPerMin, maxInsertions, numClients, startingIP, batchSize):
     startTime = time.time()
     sleepTime = getSleepTime(insertPerMin)
-    counter = 0;
-    db = setupDatabase()
-    cursor = db.cursor()
+    counter = 0
+    db = setupDatabase(CONST_DB_HOST)
+    remote_dbs = init_db_connections(startingIP, numClients)
+    queuedInserts = deque([])
 
-    while (True):
-        insertionTime = time.time()
+    while counter < maxInsertions:
+        # Gets data to insert
         key = getHash()
         value = random.randint(CONST_MIN_NUM, CONST_MAX_NUM)
-        insertIntoDB(cursor, key, value)
-        print("Inserted \"" + key + "\" with value " + str(value))
 
+        # Inserts in local DB
+        insertionTime = time.time()
+        insertIntoDB(db.cursor(), [(key, value)])
+        afterInsertionTime = time.time()
+        timeTook = afterInsertionTime - insertionTime
+        print("Inserted \"" + key + "\" with value " + str(value) + " and took " + round(timeTook, 2) + " seconds")
+
+        # Adds to queue
+        queuedInserts.append((key, value))
+
+        # Checks if need to flush queue
+        if len(queuedInserts) == batchSize:
+            insertionTime = time.time()
+            for rdb in remote_dbs:
+                insertIntoDB(rdb.cursor(), queuedInserts)
+            afterInsertionTime = time.time()
+            print("Took " + (afterInsertionTime - insertionTime) + " seconds to insert in remote databases.")
+
+        # Sleeps remaining time
         print("Will sleep " + str(sleepTime) + " seconds.")
-        time.sleep(sleepTime - insertionTime)
-        counter+=1
+        time.sleep(sleepTime - timeTook)
 
-        if counter > maxInsertions:
-            break;
+        counter += 1
 
     db.close()
-    sys.exit(0)
 
 
 # Returns a hash using SHA256 algorithm and the current UNIX timestamp
@@ -85,8 +106,17 @@ def getHash():
 
 
 # inserts the @param key and @param value into the database
-def insertIntoDB(cursor, key, value):
-    cursor.execute("INSERT INTO " + CONST_DB_TABLENAME + " VALUES (\"" + key + "\", " + str(value) + ")")
+def insertIntoDB(cursor, values):
+    insertString = "INSERT INTO " + CONST_DB_TABLENAME + " VALUES "
+    insertString += ("(\"" + values[0] + "\", " + str(values[1]) + ")")
+
+    iterValues = iter(values)
+    next(iterValues)
+
+    for v in iterValues:
+        insertString += (", (\"" + v[0] + "\", " + str(v[1]) + ")")
+
+    cursor.execute(insertString)
 
 
 # gets the average from all the tuples in the database and returns it
@@ -96,21 +126,31 @@ def getAverage(cursor):
         print(row[0])
 
 
-# connects to the database
-def setupDatabase():
-    db = MySQLdb.connect(host=CONST_DB_HOST,
+# Tries connection to a DB in IP
+def setupDatabase(ip):
+    db = MySQLdb.connect(host=str(ip),
                          user=CONST_DB_USER,
                          passwd=CONST_DB_PASSWORD,
                          db=CONST_DB_NAME)
     db.autocommit(True)
     return db
 
+# Starts connections to the other clients databases and stores the DBs in a list
+def init_db_connections(startingIP, numClients):
+    remote_dbs = []
+
+    for i in range(1, numClients):
+        remote_dbs.append(setupDatabase(startingIP + i))
+
+    return remote_dbs
+
+# Tests local database
 def testDatabase():
-    db = setupDatabase()
+    db = setupDatabase(CONST_DB_HOST)
     cursor = db.cursor()
     key = getHash()
     value = random.randint(CONST_MIN_NUM, CONST_MAX_NUM)
-    insertIntoDB(cursor, key, value)
+    insertIntoDB(cursor, [(key, value)])
     getAverage(cursor)
     sys.exit(0)
 
