6dac22589f8a46231ecda31b3ca75b1c856c8fab
Nuno Morais <nm.morais@campus.fct.unl.pt>
2018-03-22 17:10:42 +0000

    Fixed Sync problems.
    Containers now wait for the first insertion in the synced table , then they increment

--- Container/init_container.sh
+++ Container/init_container.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 echo "Starting mysql service"
 service mysql start
 echo "initializing database"
 mysql < DB_script.sql
 
-echo "starting dummy app with $1 InsPerMin and $2 timeout"
-python3 toyapp.py $1 $2 > toyapp.log
+echo "starting dummy app"
+python3 toyapp.py $1 $2 $3 $4 $5 
--- start_containers.sh
+++ start_containers.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-docker rm -f $(docker ps -a -q) # removes all containers
+docker rm -f $(docker ps -a -q) | grep "" # removes all containers
+
 
 nContainers=$1 # the number of containers to open
 insPerMin=$2 # number of insertions per minute in the python app
-timeout=$3 # the python app timeout
+maxInserts=$3 # the python app timeout
+startingIP=$4 #the IP of Container0
+batchSize=$5 #how many insertions per batch 
+
 imageName=$(cat logs/image_name.log) # fetches the image name in the logs file
-command="bash init_container.sh $insPerMin $timeout" # built command to pass to the container
+command="bash init_container.sh $insPerMin $maxInserts $nContainers $startingIP $batchSize" # built command to pass to the container
 counter=0 
 
 while [ $counter -lt $nContainers ]; do
 	containerName="Container$counter" 
 	echo "Starting $containerName" 
-	docker run -d  --name $containerName $imageName $command
+	docker run -d --name $containerName $imageName $command
 	((counter++))
 done
