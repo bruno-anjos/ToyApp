49b80f91cf5fdffd0ab4bd73724eb8d61ecfc06d
Nuno Morais <nm.morais@campus.fct.unl.pt>
2018-04-26 11:39:04 +0100

    Toyapp now reads args from cli and from file (default is cli)

--- VirtualMachines/init_VM.sh
+++ VirtualMachines/init_VM.sh
@@ -5,7 +5,4 @@ apt-get install python3-pip-y
 apt-get install mysql-server -y 
 apt-get install mysql-client -y 
 apt-get install libmysqlclient-dev -y 
-pip3 install --upgrade pip 
 pip3 install mysqlclient --upgrade pip
-
-mysql < DB_script.sql
