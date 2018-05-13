8e02fcf1a100806855f208466533f62b2fc652f6
Nuno Morais <nm.morais@campus.fct.unl.pt>
2018-05-13 18:59:26 +0100

    Added debug messages and bugfixes

--- VirtualMachines/init_VM.sh
+++ VirtualMachines/init_VM.sh
@@ -1,4 +1,8 @@
-/etc/init.d/mysql start
+echo "starting mysql service"
+service mysql start
+echo "running DB script"
 mysql < ../Containers/Container/DB_script.sql
+echo "starting toyapp....."
 python3 ../Containers/Container/toyapp.py
+echo "erasing DB"
 mysql < erase_DB.sql
