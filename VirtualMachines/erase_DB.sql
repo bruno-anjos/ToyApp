37cc7bdbcb679632e8765ab6d07582c820c99d92
Nuno Morais <nm.morais@campus.fct.unl.pt>
2018-05-13 18:41:26 +0100

    Added VM initialization scripts

---
+++ VirtualMachines/erase_DB.sql
@@ -0,0 +1,3 @@
+DROP USER if exists 'username'@'localhost'
+DROP USER if exists 'username'@'%'
+drop database if exists dummy_db;
---
+++ VirtualMachines/start_vms.sh
@@ -0,0 +1,25 @@
+nVM=$1 		 	# the number of vms to open
+insPerMin=$2 	# number of insertions per minute in the python app
+maxInserts=$3 	# the python app timeout
+startingIP=$4 	# the IP of vm0
+batchSize=$5 	# how many insertions per batch 
+rsa_key=$6
+
+ip_components=($(echo "$startingIP" | tr '.' '\n'))
+
+echo "$1 $2 $3 $4 $5" > args.txt
+
+counter=0 
+while [ $counter -lt $nVM ]; do
+
+	last_component=$((${ip_components[3]} + counter))
+	curr_IP="${ip_components[0]}.${ip_components[1]}.${ip_components[2]}.${last_component}"
+
+	scp -i $rsa_key root@$curr_IP  args.txt root@curr_IP:/root/Toyapp//Containers/Container/args.txt
+	ssh -i $rsa_key root@$curr_IP  bash /VirtualMachines/init_VM.sh
+
+	((counter++))
+
+done
+
+rm args.txt
