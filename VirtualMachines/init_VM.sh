1dde469cdae349c8c15dc9b09c0626d25bb84a6b
Nuno Morais <nm.morais@campus.fct.unl.pt>
2018-05-13 19:13:03 +0100

    Fixing path problems

--- VirtualMachines/init_VM.sh
+++ VirtualMachines/init_VM.sh
@@ -1,8 +1,8 @@
 echo "starting mysql service"
 service mysql start
 echo "running DB script"
-mysql < ../Containers/Container/DB_script.sql
+mysql < /root/Toyapp/Containers/Container/DB_script.sql
 echo "starting toyapp....."
-python3 ../Containers/Container/toyapp.py
+python3 /root/Toyapp/Containers/Container/toyapp.py
 echo "erasing DB"
 mysql < erase_DB.sql
