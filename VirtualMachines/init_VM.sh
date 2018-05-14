8923d0789046f37675fe906d9d36dc14cd19ef28
Nuno Morais <nm.morais@campus.fct.unl.pt>
2018-05-14 18:08:15 +0100

    And again

--- VirtualMachines/init_VM.sh
+++ VirtualMachines/init_VM.sh
@@ -3,6 +3,6 @@ service mysql start
 echo "running DB script"
 mysql < ../Containers/Container/DB_script.sql
 echo "starting toyapp....."
-cd ../Containers/Container/toyapp.py && python3 toyapp.py
+cd .. && cd Toyapp/Containers/Container && python3 toyapp.py
 echo "erasing DB"
 mysql < erase_DB.sql
