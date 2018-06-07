82c92570080d2d006d00fdf124b26638e095f85c
bruno-anjos <bruno.anjos96@hotmail.com>
2018-06-07 13:38:26 +0100

    Update teste remoto (por testar)

--- startDockerTests.sh
+++ startDockerTests.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
 tests_file=$1
-rsa_key=$2
-sleep_time=$3
-ip=10.170.138.198
+sleep_time=$2
 
 if [ ! -d ~/Desktop/DockerLogs/ ]; then
     mkdir ~/Desktop/DockerLogs/
 fi
 
-counter=0
+counter=1
+
+cd /root/ToyApp/Containers/
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    echo "Deleting container logs..."
-    ssh -n -i $rsa_key root@$ip rm -f "~/Desktop/ContainerLogs/*"
-    echo "Starting test $line"
-    command="cd ToyApp/Containers/ && ./start_containers.sh $line"
-    screen -d -m -L ssh -i $rsa_key root@$ip $command
-    echo "Will sleep $sleep_time"
-    sleep $sleep_time
-    mkdir ~/Desktop/DockerLogs/"Test_$counter"
-    echo "Downloading logs to ~/Desktop/DockerLogs/"Test_$counter"/"
-    scp -i $rsa_key root@$ip:~/Desktop/ContainerLogs/* ~/Desktop/DockerLogs/"Test_$counter"/ < /dev/null
+    rm -f "~/Desktop/ContainerLogs/*"
+    ./start_containers.sh ${line}
+    sleep ${sleep_time}
+    mkdir "~/Desktop/Test_"${counter}"/"
+    mv "~/Desktop/ContainerLogs/*" "~/Desktop/Test_"${counter}"/"
     ((counter++))
 done < "$tests_file"
