a58cc28b37fe9c09d5daf6fe28fa9f20e2cc8e6e
Nuno Morais <nm.morais@campus.fct.unl.pt>
2018-06-07 18:47:40 +0100

    whole container runs or ram

--- Containers/Container/init_container.sh
+++ Containers/Container/init_container.sh
@@ -2,9 +2,6 @@
 
 echo "Starting mysql service"
 
-mkdir /ramdisk/mysqldata
-mkdir /ramdisk/mysqltemp
-
 /etc/init.d/mysql start
 echo "initializing database"
 mysql < DB_script.sql
--- Containers/start_containers.sh
+++ Containers/start_containers.sh
@@ -9,16 +9,6 @@ maxInserts=$3 # the python app timeout
 startingIP=$4 #the IP of Container0
 batchSize=$5 #how many insertions per batch 
 
-
-totalmb=$((nContainers * 100))
-
-
-#set up ramdisk , giving 200m ram per container
-mkdir $(pwd)/ramdisk
-mount -t tmpfs -o size=$((totalmb))m tmpfs $(pwd)/ramdisk
-
-
-
 imageName=$(cat logs/image_name.log) # fetches the image name in the logs file
 command="bash init_container.sh $insPerMin $maxInserts $nContainers $startingIP $batchSize" # built command to pass to the container
 counter=0 
@@ -27,7 +17,7 @@ while [ $counter -lt $nContainers ]; do
 	containerName="Container$counter" 
 	echo "Starting $containerName" 
 	mkdir ramdisk/$containerName
-	docker run -d -v ~/Desktop/ContainerLogs:/log -v $(pwd)/ramdisk/$containerName:/ramdisk --name $containerName $imageName $command
+	docker run -d --tmpfs / -v ~/Desktop/ContainerLogs:/log $containerName $imageName $command
 
 	((counter++))
 done
