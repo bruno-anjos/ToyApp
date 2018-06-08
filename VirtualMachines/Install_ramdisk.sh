be5f7d75b3ad6744825901bcbed5be9e75e5211e
Nuno Morais <nm.morais@campus.fct.unl.pt>
2018-06-08 12:56:18 +0100

    ramdisk stuff

--- VirtualMachines/Install_ramdisk.sh
+++ VirtualMachines/Install_ramdisk.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm -rf /var/lib/mysql/*
+cp my.cnf /etc/my.cnf
 mkdir /ramdisk
 
 mount -t tmpfs -o size=1G tmpfs /ramdisk
@@ -8,8 +9,5 @@ mkdir /ramdisk/mysqldata
 mkdir /ramdisk/mysqltemp
 
 chmod -R 777 /ramdisk
+chown mysql:mysql /ramdisk
 
-echo "[mysqld]" >> /etc/mysql/my.cnf	
-echo "innodb_use_native_aio = 0" >> /etc/mysql/my.cnf
-echo "datadir = /ramdisk/mysqldata" >> /etc/mysql/my.cnf
-echo "tmpdir = /ramdisk/mysqltemp" >> /etc/mysql/my.cnf
