f3edb25fd4894e130b24a4ba8e951ab5a359a6b7
Nuno Morais <nm.morais@campus.fct.unl.pt>
2018-06-08 10:11:37 +0100

    changed paths

--- startDockerTests.sh
+++ startDockerTests.sh
@@ -4,14 +4,14 @@ tests_file=$1
 sleep_time=$2
 
 counter=1
-
+mkdir /root/ContainerLogs/
 cd /root/ToyApp/Containers/
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    rm -f "/root/ContainerLogs/Temp/*"
+    rm -f "/root/Desktop/ContainerLogs/*"
     ./start_containers.sh ${line}
     sleep ${sleep_time}
     mkdir "/root/ContainerLogs/Test_"${counter}
-    mv "/root/ContainerLogs/Temp/*" "/root/ContainerLogs/Test_"${counter}"/"
+    mv "/root/Desktop/ContainerLogs/*" "/root/ContainerLogs/Test_"${counter}"/"
     ((counter++))
 done < "$tests_file"
