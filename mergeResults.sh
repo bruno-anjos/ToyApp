6d66b407803a6a66cb6d58cbbbfa6a76ffe2eebc
Nuno Morais <nm.morais@campus.fct.unl.pt>
2018-06-10 00:33:50 +0100

    some more

--- mergeResults.sh
+++ mergeResults.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-tests_folder=$1
+logs_folder=$1
 filterResultsPath=$2
 
-rm ${tests_folder}/merged_results.csv
+rm ${logs_folder}/merged_results.csv
 total=0
 
-for folder in ${tests_folder}/*/; do
+for folder in ${logs_folder}/*/; do
     echo "Filtering results for folder ${folder}"
     ${filterResultsPath} ${folder}
     ((total++))
 done
 
-
+cd $logs_folder
 counter=1
 while [ $counter -lt $total ]; do
 	file=Test_$counter/test.csv
-    echo "Merging ${file} with ${tests_folder}merged_results.csv..."
-    cat $file >> $tests_folder/merged_results.csv
-    printf "\n" >> $tests_folder/merged_results.csv
+    echo "Merging ${file} with ${logs_folder}merged_results.csv..."
+    cat $file >> $logs_folder/merged_results.csv
+    printf "\n" >> $logs_folder/merged_results.csv
     ((counter++))
 done
