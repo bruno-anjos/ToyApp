18ec5743fe6dd72609f5500abf7453f3da225200
Nuno Morais <nm.morais@campus.fct.unl.pt>
2018-06-11 10:43:47 +0100

    ramdisk stuff

--- VirtualMachines/Install_ramdisk.sh
+++ VirtualMachines/Install_ramdisk.sh
@@ -4,6 +4,6 @@ rm -rf /var/lib/mysql
 
 mkdir  /var/lib/mysql
 mount -t tmpfs -o size=1G tmpfs /var/lib/mysql/
-chmod 600 /var/lib/mysql
-chown mysql:mysql /var/lib/mysql
+chmod -R 600 /var/lib/mysql
+chown -R mysql:mysql /var/lib/mysql
 
--- VirtualMachines/dependencies.sh
+++ VirtualMachines/dependencies.sh
@@ -4,6 +4,7 @@ yum install -y http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch
 yum update -y
 yum install -y mysql 
 yum install -y mysql-devel 
+yum install -y mariadb-server
 yum install -y python34-devel
 yum install -y python34-setuptools
 yum install -y git
@@ -13,3 +14,4 @@ sudo easy_install-3.4 pip
 pip3 install mysqlclient
 git clone https://github.com/bruno-anjos/ToyApp.git
 
+./install_ramdisk
