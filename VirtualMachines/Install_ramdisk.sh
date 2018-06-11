a1d9044aedae1bfe5871781706617e7cffd6b96f
Nuno Morais <nm.morais@campus.fct.unl.pt>
2018-06-11 10:15:11 +0100

    1

--- VirtualMachines/Install_ramdisk.sh
+++ VirtualMachines/Install_ramdisk.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-rm -rf /var/lib/mysql/*
+rm -rf /var/lib/mysql
 
+mkdir  /var/lib/mysql
 mount -t tmpfs -o size=1G tmpfs /var/lib/mysql/
-chmod 600 /var/lib/mysql/
-chown mysql:mysql /var/lib/mysql/
+chmod 600 /var/lib/mysql
+chown mysql:mysql /var/lib/mysql
 
