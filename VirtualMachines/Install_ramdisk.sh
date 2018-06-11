e26b6b051d72ffa1f0c1a7cd89d76a42aa2d76fb
Nuno Morais <nm.morais@campus.fct.unl.pt>
2018-06-11 10:09:20 +0100

    cenas

--- VirtualMachines/Install_ramdisk.sh
+++ VirtualMachines/Install_ramdisk.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 rm -rf /var/lib/mysql/*
-cp my.cnf /etc/my.cnf
-mkdir /ramdisk
 
-mount -t tmpfs -o size=1G tmpfs /ramdisk
-mkdir /ramdisk/mysqldata
-mkdir /ramdisk/mysqltemp
-
-chmod -R 777 /ramdisk
-chmod 600 /etc/my.cnf
-chown mysql:mysql /ramdisk
+mount -t tmpfs -o size=1G tmpfs /var/lib/mysql/
+chmod 600 /var/lib/mysql/
+chown mysql:mysql /var/lib/mysql/
 
