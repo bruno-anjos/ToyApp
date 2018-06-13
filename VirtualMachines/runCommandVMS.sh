afeb27f9848bf6ed2fd3c9fb896464c80c46c339
Nuno Morais <nm.morais@campus.fct.unl.pt>
2018-06-13 14:35:21 +0100

    update

--- VirtualMachines/runCommandVMS.sh
+++ VirtualMachines/runCommandVMS.sh
@@ -15,7 +15,7 @@ while [ $counter -lt $nVM ]; do
 	last_component=$((${ip_components[3]} + counter))
 	curr_IP="${ip_components[0]}.${ip_components[1]}.${ip_components[2]}.${last_component}"
 
-	echo "copying args to: $curr_IP"
+	echo "running ${command} on ${curr_IP}"
 
 	ssh -i $rsa_key root@$curr_IP $command
 
