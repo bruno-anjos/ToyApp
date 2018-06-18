340e140f3fb8c94819d07b466e4c6a15ecb4b37b
Nuno Morais <nm.morais@campus.fct.unl.pt>
2018-06-18 02:14:33 +0100

    ramdisk size

--- VirtualMachines/install_ramdisk.sh
+++ VirtualMachines/install_ramdisk.sh
@@ -4,7 +4,7 @@ systemctl stop mariadb
 rm -rf /var/lib/mysql
 
 mkdir  /var/lib/mysql
-mount -t tmpfs -o size=1G tmpfs /var/lib/mysql/
+mount -t tmpfs -o size=2512m tmpfs /var/lib/mysql/
 chmod -R 600 /var/lib/mysql
 chown -R mysql:mysql /var/lib/mysql
 systemctl start mariadb
